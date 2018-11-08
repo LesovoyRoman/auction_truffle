a7d5a5dca63ba6606e3dd78a777dc9f5192d83e3
Roman <romalesov@ukr.net>
2018-11-08 11:00:44 +0200

    -- TESTS -- -> created test of Auction

---
+++ test/Auction.test.js
@@ -0,0 +1,125 @@
+const assert = require('assert');
+
+const ganache = require('ganache-cli');
+
+const Web3 = require('web3');
+
+const web3 = new Web3(ganache.provider());
+
+const json = require('./../build/contracts/Auction.json');
+
+let accounts;
+
+let auction;
+
+let manager;
+
+const interface = json['abi'];
+
+const bytecode = json['bytecode'];
+
+beforeEach(async () => {
+
+    accounts = await web3.eth.getAccounts();
+
+    manager = accounts[0];
+
+    auction = await new web3.eth.Contract(interface)
+
+        .deploy({ data: bytecode })
+
+        .send({ from: manager, gas: '1000000' });
+});
+
+describe('Auction', () => {
+   it('deploy a contract', async () => {
+      const auctionManager = await auction.methods.manager().call();
+
+      assert.equal(manager, auctionManager, 'The manager is the one who launches the smart contract');
+   });
+
+   it('auction the item', async () => {
+
+       seller = accounts[1];
+
+       await auction.methods.auction(2).send({ from: seller });
+
+       auctionSeller = await auction.methods.seller().call();
+
+       assert.equal(auctionSeller, seller, 'The seller is the one who called the auction method.');
+
+       auctionBid = await auction.methods.latestBid().call();
+
+       assert.equal(auctionBid, web3.utils.toWei('2', 'ether'), 'The latest bid is the argument sent to auction method converted into wei.')
+
+   });
+
+   it('bids the item', async () => {
+
+       bidder = accounts[2];
+
+       await auction.methods.bid().send({ from: bidder, value: web3.utils.toWei('3', 'ether') });
+
+       auctionBid = await auction.methods.latestBid().call();
+
+       assert.equal(auctionBid, web3.utils.toWei('3', 'ether'), 'The latest bid is the payment sent to bid method converted into wei.');
+
+   });
+
+   it('must bid above latest bid amount', async () => {
+
+        bidder = accounts[3];
+
+        try {
+
+            await auction.methods.bid().send({ from: bidder, value: web.utils.toWei('1', 'ether') })
+
+            assert(false);
+
+        } catch (err) {
+
+            assert(err);
+
+        }
+   });
+
+   it('bids one more', async () => {
+
+       bidderNew = accounts[4];
+
+       await auction.methods.bid().send({ from: bidderNew, value: web3.utils.toWei('4', 'ether')});
+
+       auctionBid = await auction.methods.latestBid().call();
+
+       assert.equal(auctionBid, web3.utils.toWei('4', 'ether'), 'Latest new bid sent to bid method converted to wei');
+
+   });
+
+   it('only manager can finish the auction', async () => {
+
+       nonmanager = accounts[1];
+
+       try {
+
+           await auction.methods.finishAuction().send({ from: nonmanager });
+
+           assert(false);
+
+       } catch (err) {
+
+           assert(err);
+
+       }
+
+   });
+
+   it('finishes the auction as manager', async () => {
+
+       manager = accounts[0];
+
+       await auction.methods.finishAuction().send({ from: manager });
+
+       assert(true);
+
+   })
+});
