1827d4761c81bcb5bd01f9c0d157bec0549ea6d3
Roman <romalesov@ukr.net>
2018-11-13 09:55:11 +0200

    -- Contracts -- -> Iterator

---
+++ contracts/Iterator.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.24;
+
+contract Iterator {
+
+    address creator;
+    uint8[10] integers;
+
+    constructor() public {
+        creator = msg.sender;
+        uint8 x = 0;
+        while(x < integers.length) {
+            integers[x] = x;
+            x++;
+        }
+    }
+
+    function getSum() public view returns (uint256) {
+        require(msg.sender == creator, "Unathorized.");
+        uint8 x = 0;
+        uint256 sum = 0;
+        while(x < integers.length) {
+            sum += integers[x];
+            x++;
+        }
+
+        return sum;
+    }
+
+}
